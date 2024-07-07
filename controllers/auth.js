689536decd9c013a7ca6af90ee3b31b0f9159f66
Manel <walletdans@gmail.com>
2024-07-07 19:56:01 +0200

    last commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,26 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors/bad-request')
+// User.create: es un metodo de mongoose que se puede utilizar a partir de un esquema. En este caso User.
+
 const register = async (req, res) => {
-    res.send('You just created an account1')
+    try {
+        const {name,email,password} = req.body
+    if(!name || !email || !password) {
+        throw new BadRequestError('Please provide name,email and password')
+    }
+        const user = await User.create({ ...req.body })
+    res.status(StatusCodes.CREATED).json({ user })
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+
 }
 
 const login = async (req, res) => {
-    res.send('You just been loged in')
+    res.send('login user')
 }
 
 module.exports = {
