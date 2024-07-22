422313dda0ebefddc09b3ea12da33cafd76effb5
Manel <walletdans@gmail.com>
2024-07-22 17:54:37 +0200

    last commit

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -37,11 +37,53 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send('update job');
+    try {
+        const {
+            body: {company,position},
+            user: {userId},
+            params:{id:jobId}
+        } = req
+    
+        if(company === '' || position === ''){
+            throw new BadRequestError('company or Position fields cannot be empty')
+        }
+    
+        const job = await Job.findByIdAndUpdate(
+            {
+            _id: jobId,
+            createdBy:userId
+            }, req.body, { new:true, runValidators: true})
+    
+        if(!job){
+            throw new NotFoundError(`No job with id ${jobId}`)
+          }
+        
+          res.status(StatusCodes.OK).json({job})
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+    
 };
 
 const deleteJob = async (req, res) => {
-  res.send('delete job');
+    const {
+        body: {company,position},
+        user: {userId},
+        params:{id:jobId}
+    } = req
+
+    const job = await Job.findByIdAndDelete({
+        _id: jobId,
+        createdBy: userId
+    })
+    
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+      }
+    
+      res.status(StatusCodes.OK).json({job})
 };
 
 module.exports = {
